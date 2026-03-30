b2ffef02328f06a4eb1c10ad1b65e28932eccdd1
shahadat-hossain99 <shahadathossain31500@gmail.com>
2026-03-30 14:54:05 +0600

    theme set to ligth mode and favicon replaced

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1>Welcome to my Digital React world</h1>
+
+      <header></header>
+      <main></main>
+      <footer></footer>
     </>
   );
 }
