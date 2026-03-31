a2f1737d3a71c799f3cf4810a36e8bd35f4fbc09
shahadat-hossain99 <shahadathossain31500@gmail.com>
2026-03-31 21:13:33 +0600

    Remove button function added and total price shown in the ui

--- src/Components/CartProducts/CartProducts.jsx
+++ src/Components/CartProducts/CartProducts.jsx
@@ -1,16 +1,45 @@
 import React from "react";
 import { CiShoppingCart } from "react-icons/ci";
+import { toast } from "react-toastify";
 
 const CartProducts = ({ cart, setCart }) => {
   //   console.log(cart);
+
+  let total = 0;
+  cart.forEach((item) => {
+    total += item.price;
+  });
+
+  const handlePayment = () => {
+    setCart([]);
+    toast.success("Payment Successful");
+  };
+
+  const handleRemoveProduct = (item) => {
+    console.log(cart);
+    const filteredProducts = cart.filter(
+      (product) => product.name !== item.name,
+    );
+    console.log("filteredProducts", filteredProducts);
+    toast.error("Product Removed", {
+      position: "top-center",
+      autoClose: 3000,
+      theme: "colored",
+    });
+    setCart(filteredProducts);
+  };
+
   return (
-    <div className="border p-10 border-gray-300 rounded-2xl my-10">
+    <div className="border p-2 md:p-10 border-gray-300 rounded-2xl my-10">
       <h2 className="text-xl font-bold mb-4">Your Cart</h2>
       {cart.length ? (
         <div>
           <div className="space-y-4 ">
             {cart.map((item) => (
-              <div className="flex justify-between items-center bg-base-200 p-5 rounded-2xl transform transition-all duration-300 ease-in-out hover:scale-102 hover:shadow-xl hover:border border-pink-300">
+              <div
+                key={item.id}
+                className="flex justify-between items-center bg-base-200 p-5 rounded-2xl transform transition-all duration-300 ease-in-out hover:scale-102 hover:shadow-xl hover:border border-pink-300"
+              >
                 <div className="flex items-center gap-4">
                   <div className="flex">
                     <div className="p-4 border border-gray-200 rounded-full bg-white">
@@ -23,15 +52,27 @@ const CartProducts = ({ cart, setCart }) => {
                     <p className="text-gray-400">${item.price}</p>
                   </div>
                 </div>
-                <button className="btn btn-ghost text-pink-600">Remove</button>
+                <button
+                  onClick={() => {
+                    handleRemoveProduct(item);
+                  }}
+                  className="btn btn-ghost text-pink-600"
+                >
+                  Remove
+                </button>
               </div>
             ))}
           </div>
           <div className="flex justify-between items-center py-6">
             <h2 className="text-2xl text-gray-400 font-bold">Total:</h2>
-            <h2 className="text-2xl font-bold">$100</h2>
+            <h2 className="text-2xl font-bold">${total}</h2>
           </div>
-          <button className="btn h-14 w-full bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white rounded-full text-lg">
+          <button
+            onClick={() => {
+              handlePayment();
+            }}
+            className="btn h-14 w-full bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white rounded-full text-lg"
+          >
             Proceed to Checkout
           </button>
         </div>
